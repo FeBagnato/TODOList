b2ffc173e652e3dc0e5a286e460a5123c8f61e56
FeBagnato <felipebag@protonmail.com>
2022-02-03 19:49:27 -0300

    :truck: Renaming classes and some other changes
    - Renaming activity_sobre.xml to fragment_sobre.xml
    - Turning SobreActivity into a fragment

---
+++ app/src/main/java/felipeBagnato/personal/todoList/ui/sobre/SobreFragment.kt
@@ -0,0 +1,8 @@
+package felipeBagnato.personal.todoList.ui.sobre
+
+import androidx.fragment.app.Fragment
+import felipeBagnato.personal.todoList.R
+
+class SobreFragment : Fragment(R.layout.activity_sobre){
+
+}
