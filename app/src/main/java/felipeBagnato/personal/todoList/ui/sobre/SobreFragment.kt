83f8d46ae62d5fb219eb5ea37b5b16bcb0dfdacd
FeBagnato <felipebag@protonmail.com>
2022-02-20 19:33:34 -0300

    :recycle: code cleaning
    - Removing an unused activity tag from AndroidManifest
    - Removing the redundant body from SobreFragment
    - Removing the unused namespace tag in nav_header_main

--- app/src/main/java/felipeBagnato/personal/todoList/ui/sobre/SobreFragment.kt
+++ app/src/main/java/felipeBagnato/personal/todoList/ui/sobre/SobreFragment.kt
@@ -3,6 +3,4 @@ package felipeBagnato.personal.todoList.ui.sobre
 import androidx.fragment.app.Fragment
 import felipeBagnato.personal.todoList.R
 
-class SobreFragment : Fragment(R.layout.fragment_sobre){
-
-}
+class SobreFragment : Fragment(R.layout.fragment_sobre)
