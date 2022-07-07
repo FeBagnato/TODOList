d7a78a34ed8a06eaf66b3245c51cade98d14efed
FeBagnato <felipebag@protonmail.com>
2022-07-07 10:39:35 -0300

    Adding a static class to storage constants values for sorting types

---
+++ app/src/main/java/felipeBagnato/personal/todoList/constants/SortingConstants.kt
@@ -0,0 +1,10 @@
+package felipeBagnato.personal.todoList.constants
+
+abstract class SortingConstants{
+    companion object{
+        const val MAIS_ANTIGO = 1
+        const val MAIS_NOVO = 2
+        const val DATA_LIMITE = 3
+        const val ALFABETICA = 4
+    }
+}
